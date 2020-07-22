cf3b53f193673632249c6529e7b26873ff086fbb
kangsh9107 <68472349+kangsh9107@users.noreply.github.com>
2020-07-22 22:36:08 +0900

    Daily Tutorial5 & Daily Tutorial p5

---
+++ Tutorial5/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String a = "I Love You.";
+		if(a.contains("Love"))
+		{
+		// 포함하는 경우 실행되는 부분
+		System.out.println("Me Too.");
+			
+		}
+		else
+		{
+		// 포함하지 않는 경우 실행되는 부분
+		System.out.println("I Hate You.");
+		}
+	}
+
+}
