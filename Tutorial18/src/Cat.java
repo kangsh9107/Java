e6f594d1958f234cc14404579d301b7afa18fde3
kangsh9107 <68472349+kangsh9107@users.noreply.github.com>
2020-08-05 22:44:09 +0900

    Daily Tutorial18,19 & Daily Tutorial p18,19

---
+++ Tutorial18/src/Cat.java
@@ -0,0 +1,12 @@
+
+public class Cat extends Animal {
+
+	@Override
+	void crying() {
+
+		System.out.println("¾ß¿Ë~");
+		
+	}
+	
+
+}
---
+++ Tutorial18/src/Player.java
@@ -0,0 +1,8 @@
+
+abstract class Player {
+	abstract void play(String songName);
+	abstract void pause();
+	abstract void stop();
+	
+
+}
