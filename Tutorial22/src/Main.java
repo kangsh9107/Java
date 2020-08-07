33255a29551b05bf6a5d4ec6acb006d119122e88
kangsh9107 <68472349+kangsh9107@users.noreply.github.com>
2020-08-07 22:58:47 +0900

    Daily Tutorial22,23 & Daily Tutorial p22,23

---
+++ Tutorial22/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Archer archer1 = new Archer("±Ã¼ö1", "»ó");
+		Archer archer2 = new Archer("±Ã¼ö1", "»ó");
+		System.out.println(archer1 == archer2);
+		System.out.println(archer1.equals(archer2));
+
+	}
+
+}
